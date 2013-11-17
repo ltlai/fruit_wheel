35fbdc2def56356c7f7d54cd7531175fca388415
ltlai <leslie.t.lai@gmail.com>
2013-11-16 18:31:24 -0800

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Fruit.create(name: "Apple", serving_size: "1 Large", calories: 130, potassium: 7, 
+  carbs: 11, fiber: 20, sugar: 25, vitamin_a: 2, vitamin_c: 8, 
+  image_url: '/images/fruitwheel-01.jpg')
+
+Fruit.create(name: "Avocado", serving_size: "1/5 Medium", calories: 50, potassium: 4, 
+  carbs: 1, fiber: 4, sugar: 0, vitamin_a: 0, vitamin_c: 4, 
+  image_url: '/images/fruitwheel-02.jpg')
+
+Fruit.create(name: "Banana", serving_size: "1 Medium", calories: 110, potassium: 13, 
+  carbs: 10, fiber: 12, sugar: 19, vitamin_a: 2, vitamin_c: 15, 
+  image_url: '/images/fruitwheel-03.jpg')
+
+Fruit.create(name: "Cherimoya", serving_size: "3.5 oz", calories: 75, potassium: 6, 
+  carbs: 10, fiber: 100, sugar: 13, vitamin_a: 0, vitamin_c: 15, 
+  image_url: '/images/fruitwheel-04.jpg')
+
+Fruit.create(name: "Dragon Fruit", serving_size: "1 Large", calories: 100, potassium: 100, 
+  carbs: 100, fiber: 100, sugar: 100, vitamin_a: 100, vitamin_c: 100, 
+  image_url: '/images/fruitwheel-05.jpg')
+
+Fruit.create(name: "Passion Fruit", serving_size: "1", calories: 100, potassium: 100, 
+  carbs: 100, fiber: 100, sugar: 100, vitamin_a: 100, vitamin_c: 100, 
+  image_url: '/images/fruitwheel-06.jpg')
+
+Fruit.create(name: "Kumquat", serving_size: "1 Large", calories: 100, potassium: 100, 
+  carbs: 100, fiber: 100, sugar: 100, vitamin_a: 100, vitamin_c: 100, 
+  image_url: '/images/fruitwheel-07.jpg')
+
+Fruit.create(name: "Starfruit", serving_size: "1 Large", calories: 100, potassium: 100, 
+  carbs: 100, fiber: 100, sugar: 100, vitamin_a: 100, vitamin_c: 100, 
+  image_url: '/images/fruitwheel-08.jpg')
+
+Fruit.create(name: "Mangosteen", serving_size: "1 Large", calories: 100, potassium: 100, 
+  carbs: 100, fiber: 100, sugar: 100, vitamin_a: 100, vitamin_c: 100, 
+  image_url: '/images/fruitwheel-09.jpg')
+
+Fruit.create(name: "Physalis", serving_size: "1 Large", calories: 100, potassium: 100, 
+  carbs: 100, fiber: 100, sugar: 100, vitamin_a: 100, vitamin_c: 100, 
+  image_url: '/images/fruitwheel-10.jpg')
